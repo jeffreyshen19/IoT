055ac87a289cfa14c90f36dadb69f3b5e499f835
Jeffrey <jshen@andover.edu>
2017-06-22 19:00:32 +0000

    temperature

--- Weather Station/weather.c
+++ Weather Station/weather.c
@@ -2,17 +2,18 @@
 #include <unistd.h>
 #include <mraa/aio.h>
 #include <mraa.h>
+#include <math.h>
 
 int main(){
   int R0 = 100000;
-  int B = 4275
+  int B = 4275;
   mraa_aio_context thermometer;
-  float thermometer_value = 0;
+  float thermometer_value = 0.0;;
   thermometer = mraa_aio_init(0);
   
   while(1){
-    thermometer_value = 1.0/(log(1023.0/mraa_aio_readFloat(thermometer)-1.0)/B+1/298.15)-273.15;
-    printf("Temperature is: %d\n", thermometer_value);
+    thermometer_value = 1.0/(log(1023.0/mraa_aio_read_float(thermometer)-1.0)/B+1/298.15)-273.15;
+    printf("Temperature is: %.5f\n", thermometer_value);
     usleep(100000);
   }
 
