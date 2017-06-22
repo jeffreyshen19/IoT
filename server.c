59c7bf56bb378a9d25fefd79b7906bb9958e6e64
rgoggins <ryanngoggins@gmail.com>
2017-06-22 10:07:47 -0700

    can take 2 inputs/multiple messages

--- server.c
+++ server.c
@@ -16,46 +16,92 @@ void error(const char *msg)
 
 int main(int argc, char *argv[])
 {
+    
+    //argc is number of arguments
+    
+    //port: 5000
 	int server_socket_fd, client_socket_fd, portno;
 	socklen_t clilen;
 	char buffer[256];
 	struct sockaddr_in serv_addr, cli_addr;
 	int n;
+    
+    
+    //port1: 5001
+    int server_socket_fd1, client_socket_fd1, portno1;
+    socklen_t clilen1;
+    char buffer1[256];
+    struct sockaddr_in serv_addr1, cli_addr1;
+    int n1;
 
-	// error check command line arguments
-	if (argc < 2) {
-		fprintf(stderr,"ERROR, no port provided\n");
-		exit(1);
-	}
-
+    
 	// setup socket
 	server_socket_fd = socket(AF_INET, SOCK_STREAM, 0);
 	if (server_socket_fd < 0) {	
 		error("ERROR opening socket");
 	}
+    
+    //setup socket1
+    server_socket_fd1 = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_socket_fd1 < 0) {
+        error("ERROR opening socket1");
+    }
 
+    
 	// setup server information
 	memset((char *) &serv_addr, 0, sizeof(serv_addr));
 	portno = atoi(argv[1]);
 	serv_addr.sin_family = AF_INET;
 	serv_addr.sin_addr.s_addr = INADDR_ANY;
 	serv_addr.sin_port = htons(portno);
+    
+    //setup server1 information
+    memset((char*) &serv_addr1, 0, sizeof(serv_addr1));
+    portno1 = atoi(argv[2]); //ARGUMENT 2 IS SECOND PORT
+    serv_addr1.sin_family = AF_INET;
+    serv_addr1.sin_addr.s_addr = INADDR_ANY;
+    serv_addr1.sin_port = htons(portno1);
+    
+    
 
 	// bind the socket to an address
 	if (bind(server_socket_fd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
 		error("ERROR on binding");
 	}
+    
+    //bind the second socket to an address
+    if (bind(server_socket_fd1, (struct sockaddr *) &serv_addr1, sizeof(serv_addr1)) < 0) {
+        error("ERROR on binding 1");
+    }
+    
+    
 
 	// listen for incoming connections
 	// accept at most 5 connections before refusing them
 	listen(server_socket_fd, 5);
 	clilen = sizeof(cli_addr);
 
+    //listen for incoming connections
+    listen(server_socket_fd1, 5);
+    clilen1 = sizeof(cli_addr1);
+    
+    
+    
 	// block until there is a new connection. When one is received, accept it
 	client_socket_fd = accept(server_socket_fd, (struct sockaddr *) &cli_addr, &clilen);
 	if (client_socket_fd < 0) {
 		error("ERROR on accept");
 	}
+    
+    //block until new connex1
+    client_socket_fd1 = accept(server_socket_fd1, (struct sockaddr *) &cli_addr1, &clilen1);
+    if (client_socket_fd1 < 0) {
+        error("ERROR on accept 1");
+    }
+    
+    
+    
+    
     while (1) {
         
         // clear the buffer
@@ -65,8 +111,18 @@ int main(int argc, char *argv[])
             error("ERROR reading from socket");
         }
         
+        memset(buffer1, 0, 256);
+        n1 = read(client_socket_fd1, buffer1, 255);
+        if (n1 < 0) {
+            error("ERROR reading from socket");
+        }
+        
+        printf("Here is the message from connection 1: %s\n", buffer1);
+        
         // print the message to console
-        printf("Here is the message: %s\n",buffer);
+        printf("Here is the message from connection 0: %s\n",buffer);
+        
+        
         
         // send an acknowledgement back to the client saying that we received the message
         n = write(client_socket_fd, "I got your message", 18);
@@ -74,8 +130,15 @@ int main(int argc, char *argv[])
             error("ERROR writing to socket");
         }
         
+        n1 = write(client_socket_fd1, "I got your message", 18);
+        if (n1 < 0) {
+            error("ERROR writing to socket");
+        }
+        
     }
 	close(client_socket_fd);
 	close(server_socket_fd);
+    close(client_socket_fd1);
+    close(server_socket_fd1);
 	return 0; 
 }
