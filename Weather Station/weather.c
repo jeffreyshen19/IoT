ebeb1532a04df2da2282864a5966f8532079e070
Jeffrey Shen <jshen@andover.edu>
2017-06-22 11:45:58 -0700

    temperature conversion

--- Weather Station/weather.c
+++ Weather Station/weather.c
@@ -4,11 +4,14 @@
 #include <mraa.h>
 
 int main(){
+  int R0 = 100000;
+  int B = 4275
   mraa_aio_context thermometer;
-  uint16_t thermometer_value = 0;
+  float thermometer_value = 0;
   thermometer = mraa_aio_init(0);
+  
   while(1){
-    thermometer_value = mraa_aio_read(thermometer);
+    thermometer_value = 1.0/(log(1023.0/mraa_aio_readFloat(thermometer)-1.0)/B+1/298.15)-273.15;
     printf("Temperature is: %d\n", thermometer_value);
     usleep(100000);
   }
