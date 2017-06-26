f755d06ae4e175f0dbf2f527dff5cc301f73c258
rgoggins <ryanngoggins@gmail.com>
2017-06-26 13:54:45 -0700

    Update weatherServerTLS.c

--- Weather Station/weatherServerTLS.c
+++ Weather Station/weatherServerTLS.c
@@ -55,6 +55,28 @@ int create_socket(int port)
 	return s;
 }
 
+void ShowCerts(SSL* ssl)
+{   X509 *cert;
+    char *line;
+
+    cert = SSL_get_peer_certificate(ssl);	/* Get certificates (if available) */
+    if ( cert != NULL )
+    {
+        printf("Server certificates:\n");
+        line = X509_NAME_oneline(X509_get_subject_name(cert), 0, 0);
+        printf("Subject: %s\n", line);
+        free(line);
+        line = X509_NAME_oneline(X509_get_issuer_name(cert), 0, 0);
+        printf("Issuer: %s\n", line);
+        free(line);
+        X509_free(cert);
+    }
+    else
+        printf("No certificates.\n");
+}
+
+
+
 void init_openssl()
 {
 	printf("2\n");
