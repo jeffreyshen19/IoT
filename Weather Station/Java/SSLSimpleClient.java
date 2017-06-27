07b458e6ea0f6214fbe0b6d1bc53b2d1fadfb860
jeffreyshen19 <jshen@andover.edu>
2017-06-27 10:42:48 -0700

    weather station

--- Weather Station/Java/SSLSimpleClient.java
+++ Weather Station/Java/SSLSimpleClient.java
@@ -3,35 +3,66 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.UnknownHostException;
+import mraa.Aio;
 
 import javax.net.ssl.SSLSocket;
 
 
 public class SSLSimpleClient {
 
-        public static void main(String[] args) {
+  static {
+    try {
+      System.loadLibrary("mraajava");
+    } catch (UnsatisfiedLinkError e) {
+      System.err.println(
+      "Native code library failed to load. See the chapter on Dynamic Linking Problems in the SWIG Java documentation for help.\n" +
+      e);
+      System.exit(1);
+    }
+  }
 
-                SSLSocket sslSocket = null;
-                SSLClientSocket mSSLClientSocket = new SSLClientSocket(args[0], Integer.parseInt(args[1]));
-                if(mSSLClientSocket.checkAndAddCertificates()) {
-                        sslSocket = mSSLClientSocket.getSSLSocket();
-                }
-                else {
-                        return;
-                }
+  public static void main(String[] args) {
 
-                try {
-                    BufferedReader br = new BufferedReader(new InputStreamReader(sslSocket.getInputStream()));
-                    PrintWriter pw = new PrintWriter(sslSocket.getOutputStream());
-                    pw.println("Simple Message Hi...");
-                    System.out.println("Simple Message Hi... was sent");
-                    pw.flush();
+    SSLSocket sslSocket = null;
+    SSLClientSocket mSSLClientSocket = new SSLClientSocket(args[0], Integer.parseInt(args[1]));
+    if(mSSLClientSocket.checkAndAddCertificates()) {
+      sslSocket = mSSLClientSocket.getSSLSocket();
+    }
+    else {
+      return;
+    }
 
-                    System.out.println(br.readLine() + " was received");
-                    sslSocket.close();
-                } catch (Exception e) {
-                        e.printStackTrace();
-                }
-        }
+    try {
+      BufferedReader br = new BufferedReader(new InputStreamReader(sslSocket.getInputStream()));
+      PrintWriter pw = new PrintWriter(sslSocket.getOutputStream());
+      pw.println("Simple Message Hi...");
+      System.out.println("Simple Message Hi... was sent");
+      pw.flush();
+
+      Aio thermometer = new Aio(0);
+
+      int R0 = 100000;
+      int B = 4275;
+      
+      while(true){
+        int temperature = thermometer.readFloat();
+        int R = (1023.0 / temperature - 1.0) * R0;
+        temperature = 1.0/(Math.log(R/R0)/B+1/298.15)-263.15;
+
+        pw.println("" + temperature);
+        System.out.println("" + temperature);
+        pw.flush();
+
+        System.out.println(br.readLine() + " was received");
+
+        Thread.sleep(1000);
+      }
+
+      sslSocket.close();
+
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
 
 }
