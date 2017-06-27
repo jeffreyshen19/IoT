19b33c39c391a863b116f811d9ab197bd8576826
jeffreyshen19 <jshen@andover.edu>
2017-06-27 13:36:46 -0700

    added commands

--- Weather Station/Java/SSLSimpleClient.java
+++ Weather Station/Java/SSLSimpleClient.java
@@ -10,9 +10,9 @@ import mraa.IntelEdison;
 import mraa.mraa;
 import mraa.Platform;
 import mraa.Result;
-
 import javax.net.ssl.SSLSocket;
-
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 public class SSLSimpleClient {
 
@@ -41,27 +41,66 @@ public class SSLSimpleClient {
     try {
       BufferedReader br = new BufferedReader(new InputStreamReader(sslSocket.getInputStream()));
       PrintWriter pw = new PrintWriter(sslSocket.getOutputStream());
-      pw.println("Simple Message Hi...");
-      System.out.println("Simple Message Hi... was sent");
+      pw.println("Initiating connection from the client");
+      System.out.println("\033[1m\033[32mSuccessfully connected to secure server\033[1m");
       pw.flush();
+      br.readLine();
 
       Aio thermometer = new Aio(0);
 
       int R0 = 100000;
       int B = 4275;
+      int temperature, double R;
+
+      Calendar cal = Calendar.getInstance();
+      SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+      String formattedString = "", serverResponse = "";
+
+      //Configuration variables
+      boolean fahrenheit = true;
+      int period = 1000;
+      boolean running = true;
 
       while(true){
-        int temperature = (int) thermometer.read();
-        double R = (1023.0 / (double) temperature - 1.0) * R0;
-        temperature = (int) (1.0/(Math.log(R/R0)/B+1/298.15)-263.15);
+        if(running){
+          temperature = (int) thermometer.read();
+          R = (1023.0 / (double) temperature - 1.0) * R0;
+          temperature = (int) (1.0/(Math.log(R/R0)/B+1/298.15)-273.15);
+          if(fahrenheit) temperature = 9 * temperature / 5 + 32;
 
-        pw.println("" + temperature);
-        System.out.println("" + temperature);
-        pw.flush();
+          formattedString = "" + sdf.format(cal.getTime()) + " " + temperature;
 
-        System.out.println(br.readLine() + " was received");
+          pw.println(formattedString);
+          System.out.println(formattedString);
+          pw.flush();
+        }
 
-        Thread.sleep(1000);
+        serverResponse = br.readLine().trim();
+      
+        //Process commands
+        if(serverResponse.indexOf("SCALE=") != -1){
+          if(serverResponse.equals("SCALE=F")) fahrenheit = true;
+          else if(serverResponse.equals("SCALE=C")) fahrenheit = false;
+        }
+        else if(serverResponse.indexOf("PERIOD=") != -1){
+          period = (int) (1000 * Double.parseDouble("" + serverResponse.charAt(.indexOf("PERIOD=") + 7)));
+        }
+        else if(serverResponse.equals("OFF")){
+          System.out.println("\033[1m\033[31mSHUTTING DOWN\033[1m");
+          break;
+        }
+        else if(serverResponse.equals("START")){
+          if(!running) System.out.println("\033[1m\033[32mStarted monitoring sensors\033[1m");
+          running = true;
+        }
+        else if(serverResponse.equals("STOP")){
+          if(running) System.out.println("\033[1m\033[31mStopped monitoring sensors\033[1m");
+          running = false;
+        }
+
+        System.out.println(serverResponse);
+
+        Thread.sleep(period);
       }
 
 
