e4ceeec7897889620548e4ebe0fd9d62c027d443
jeffreyshen19 <jshen@andover.edu>
2017-06-28 15:00:18 -0700

    string formatting

--- Temperature Network/MonitoringStation.java
+++ Temperature Network/MonitoringStation.java
@@ -75,7 +75,7 @@ public class MonitoringStation {
           formattedString = "" + sdf.format(cal.getTime()) + " " + temperature;
 
           lcd.setCursor(0,0);
-          lcd.write("" + sdf.format(cal.getTime()) + " " + temperature + " \u00B0" + (fahrenheit ? "F" : "C"));
+          lcd.write("" + sdf.format(cal.getTime()) + " " + temperature + "\u00B0" + (fahrenheit ? "F" : "C"));
           lcd.setCursor(1, 0);
           lcd.write("Running");
 
@@ -85,7 +85,7 @@ public class MonitoringStation {
         }
         else{
           lcd.setCursor(1, 0);
-          lcd.write("Paused");
+          lcd.write("Paused ");
         }
 
         serverResponse = br.readLine().trim();
