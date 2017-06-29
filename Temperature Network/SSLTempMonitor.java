395bbceee6fd5dd0db678ffff17e9eda3e7c7853
rgoggins <ryanngoggins@gmail.com>
2017-06-29 09:46:11 -0700

    Add files via upload

---
+++ Temperature Network/SSLTempMonitor.java
@@ -0,0 +1,154 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import javax.net.ServerSocketFactory;
+import javax.net.ssl.SSLServerSocketFactory;
+
+public class SSLTempMonitor extends Thread {
+
+  public static void main(String[] args) throws Exception {
+    ServerSocketFactory ssf = SSLServerSocketFactory.getDefault();
+    ServerSocket ss = ssf.createServerSocket(Integer.parseInt(args[0]));
+    ServerSocket ss1 = ssf.createServerSocket(Integer.parseInt(args[1])); //SECOND COMPILER FLAG
+    filename = args[2];
+    System.out.println("Ready for both...");
+    while (true) {
+      new SSLTempMonitor(ss.accept(), ss1.accept()).start();
+    }
+  }
+
+  private static String filename = "";
+  private Socket sock, sock1;
+
+  public SSLTempMonitor(Socket s, Socket s1) {
+    sock = s;
+    sock1 = s1;
+  }
+
+  public void run() {
+    boolean receiving = true, running = true, running1 = true;
+    ArrayList<String> messages = new ArrayList<>();
+    int sleepTime = 1000;
+    try {
+      while (running || running1) {
+        if (running) { //two br's , two socket's
+          BufferedReader br = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+          PrintWriter pw = new PrintWriter(sock.getOutputStream());
+          if (receiving) {
+            String data = br.readLine();
+            messages.add(data);
+            System.out.println(data + " is echoed");
+          }
+
+          BufferedReader msgTaker = new BufferedReader(new InputStreamReader(System.in));
+          long end=System.currentTimeMillis()+1500;
+          String message = "";
+          while((System.currentTimeMillis()<end)) {
+            if (msgTaker.ready())
+            message += msgTaker.readLine();
+          }
+          if (message.equals("")) {
+            System.out.println("No input");
+          }
+          else {
+            messages.add(message);
+            System.out.println("The message is " + message);
+          }
+          if (message.equals("")) {
+            pw.println(message);
+          }
+          else {
+            if (message.equals("STOP")) {
+              receiving = false;
+            }
+            else if (message.equals("OFF")) {
+              running = false;
+            }
+            else if (message.equals("START")) {
+              receiving = true;
+            }
+            else if (message.contains("PERIOD=")) {
+              sleepTime = Integer.parseInt(message.substring(message.indexOf("=") + 1))*1000;
+            }
+            messages.add(message);
+            pw.println(message);
+          }
+          pw.flush();
+        }
+        if (running1) {
+          BufferedReader br = new BufferedReader(new InputStreamReader(sock1.getInputStream()));
+          PrintWriter pw = new PrintWriter(sock1.getOutputStream());
+          if (receiving) {
+            String data = br.readLine();
+            messages.add(data);
+            System.out.println(data + " is echoed");
+          }
+
+          BufferedReader msgTaker = new BufferedReader(new InputStreamReader(System.in));
+
+          try {
+            //Thread.sleep(3000);
+          } catch (Exception e) {
+            System.out.println("couldnt sleep");
+          }
+
+          long end=System.currentTimeMillis()+1500;
+          String message = "";
+          while((System.currentTimeMillis()<end)) {
+            if (msgTaker.ready())
+            message += msgTaker.readLine();
+          }
+          if (message.equals("")) {
+            System.out.println("No input");
+          }
+          else {
+            messages.add(message);
+            System.out.println("The message is " + message);
+          }
+          if (message.equals("")) {
+            pw.println(message);
+          }
+          else {
+            if (message.equals("STOP")) {
+              receiving = false;
+            }
+            else if (message.equals("OFF")) {
+              running1 = false;
+            }
+            else if (message.equals("START")) {
+              receiving = true;
+            }
+            else if (message.contains("PERIOD=")) {
+              sleepTime = Integer.parseInt(message.substring(message.indexOf("=") + 1))*1000;
+            }
+            messages.add(message);
+            pw.println(message);
+          }
+          pw.flush();
+        }
+      }
+
+
+
+      sock1.close();
+      sock.close();
+      System.out.println("jeff-o!");
+      try{
+        PrintWriter writer = new PrintWriter(filename, "UTF-8");
+        for (int i = 0; i < messages.size(); i++){
+          writer.println(messages.get(i));
+        }
+        writer.close();
+      } catch (IOException e) {
+        // do something
+      }
+    } catch (IOException ioe) {
+      // Client disconnected
+    }
+    System.out.println("got here");
+  }
+}
